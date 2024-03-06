66041ea4050491894ce4c963b19f9fb24c1ac572
dell 5580 <dell 5580@DESKTOP-NP1E6RM>
2024-03-06 19:49:25 +0200

    ##3

---
+++ Jumia.Application/Services/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿using Jumia.Application.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumia.Application.Services
+{
+    public class CategoryService
+    {
+        private readonly ICategoryRepository _repository;
+
+        public CategoryService(ICategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+    }
+}
