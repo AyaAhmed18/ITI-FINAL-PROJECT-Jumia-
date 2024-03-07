b99473b94a089d32132a1c626120a4e749894399
Bahaa Rezgallh <brezgallh@student.eelu.edu.eg>
2024-03-07 17:33:53 +0200

    Admin_Dashbord_Bahaa

---
+++ AdminDashBoard/Controllers/ِAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminDashBoard.Controllers
+{
+    public class _ِAdminController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
