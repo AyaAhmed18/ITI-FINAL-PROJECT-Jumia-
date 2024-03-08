04ae39faa72153016e496c49ee9dd28122f4303c
Bahaa Rezgallh <brezgallh@student.eelu.edu.eg>
2024-03-08 17:17:08 +0200

    Login And AccountLayout_Bahaa

---
+++ Jumia.Dtos/AccountDtos/LoginDtos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumia.Dtos.AccountDtos
+{
+    public class LoginDtos
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+       
+    }
+}
