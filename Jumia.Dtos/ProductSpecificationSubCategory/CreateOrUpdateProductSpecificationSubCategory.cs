94fbb7c0c5be418280cbbb36e4ef7512d6c5fa98
ZYAD-ELFAKHARANY <57574285+ZYAD-ELFAKHARANY2001@users.noreply.github.com>
2024-03-25 15:31:02 +0200

    ProductSpecificationSubCategory

---
+++ Jumia.Dtos/ProductSpecificationSubCategory/CreateOrUpdateProductSpecificationSubCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumia.Dtos.ProductSpecificationSubCategory
+{
+    public class CreateOrUpdateProductSpecificationSubCategory
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int SubCategoryId { get; set; }
+        public int SpecificationId { get; set; }
+        public string Value { get; set; }
+
+        public string SpecificationName { get; set; }
+        public string SubCategoryName { get; set; }
+    }
+}
---
+++ Jumia.Dtos/ProductSpecificationSubCategory/GetAllProductSpecificationSubCategory.cs
@@ -0,0 +1,28 @@
+﻿using Jumia.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumia.Dtos.ProductSpecificationSubCategory
+{
+    public class GetAllProductSpecificationSubCategory
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int SubCategoryId { get; set; }
+        public int SpecificationId { get; set; }
+        public string Value { get; set; }
+
+        public string SpecificationName { get; set; }
+        public string SubCategoryName { get; set; }
+
+        // [ForeignKey(nameof(Specification))]
+        //  public virtual SubCategory SubCategory { get; set; }
+        //[ForeignKey(nameof(SubCategory))]
+        //public int SubCategoryId { get; set; }
+        //public virtual SubCategory SubCategory { get; set; }
+    }
+}
