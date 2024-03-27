9b6e86af6c34690fd20acaabaae60c313df58fb9
MAGDY <magdymohammed4000@gmail.com>
2024-03-27 16:40:58 +0200

    category&subcategory API

---
+++ JumiaStore/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using Jumia.Application.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JumiaStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        private readonly ICategoryService _categoryService;
+
+
+        public CategoryController(ICategoryService categoryService) 
+        {
+            _categoryService = categoryService;
+        
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAll(50, 1);
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetOne(int id)
+        {
+            var Category = await _categoryService.GetOne(id);
+            if (Category == null)
+            {
+                return Ok("not Found!");
+            }
+
+
+            return Ok(Category);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
