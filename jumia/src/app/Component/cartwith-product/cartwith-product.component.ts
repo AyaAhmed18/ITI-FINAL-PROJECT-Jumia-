9a91eed24564998a0c5543c5ed0a627efbff735e
AyaAhmed18 <yoyoahmed108@gmail.com>
2024-03-28 23:38:31 +0200

    cartDesign

---
+++ jumia/src/app/Component/cartwith-product/cartwith-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartwith-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './cartwith-product.component.html',
+  styleUrl: './cartwith-product.component.css'
+})
+export class CartwithProductComponent {
+
+}
--- jumia/src/app/app.routes.ts
+++ jumia/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { DelivaryComponent } from './Component/delivary/delivary.component';
 import { MyAccountComponent } from './Component/my-account/my-account.component';
 import { CartComponent } from './Component/cart/cart.component';
 import { authGuard } from './guards/auth.guard';
+import { CartwithProductComponent } from './Component/cartwith-product/cartwith-product.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'/Home',pathMatch:'full'},
@@ -17,5 +18,6 @@ export const routes: Routes = [
     {path:'Delivary',component:DelivaryComponent},
     {path:'MyAccount',component:MyAccountComponent , canActivate:[authGuard]},  //
     {path:'Cart',component:CartComponent},
+    {path:'CartProduct',component:CartwithProductComponent},
 
 ];
